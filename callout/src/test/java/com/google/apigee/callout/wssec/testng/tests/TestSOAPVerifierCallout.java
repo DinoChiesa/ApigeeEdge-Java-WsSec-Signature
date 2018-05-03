3d638fa72e7b8ac3c155c854eaaddf0524d85165
Dino Chiesa <dpchiesa@hotmail.com>
2018-05-02 18:49:19 -0700

    update tests to check return codes

--- callout/src/test/java/com/google/apigee/callout/wssec/testng/tests/TestSOAPVerifierCallout.java
+++ callout/src/test/java/com/google/apigee/callout/wssec/testng/tests/TestSOAPVerifierCallout.java
@@ -20,8 +20,8 @@ import com.apigee.flow.execution.ExecutionResult;
 import com.apigee.flow.message.Message;
 import com.apigee.flow.message.MessageContext;
 import com.google.apigee.callout.wssec.SOAPVerifier;
-import com.google.apigee.callout.wssec.XmlUtil;
 import com.google.apigee.callout.wssec.Signature;
+import com.google.apigee.callout.wssec.XmlUtil;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -35,6 +35,7 @@ import java.util.HashMap;
 import java.util.Map;
 import mockit.Mock;
 import mockit.MockUp;
+import org.apache.commons.codec.binary.Base64InputStream;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -107,29 +108,46 @@ public class TestSOAPVerifierCallout {
         }.getMockInstance();
     }
 
+    protected String normalizeString(String s) {
+            s = s.replaceAll("^ +","");
+            s = s.replaceAll("(\r|\n) +","\n");
+            return s.trim();
+    }
+
     private String readFile(String filename) throws java.io.IOException {
         return new String(Files.readAllBytes(Paths.get(testDataDir, filename)),
                           StandardCharsets.UTF_8);
+    }
+
+    private InputStream fileToInputStream(String filename) throws java.io.IOException {
+        String fileData = readFile(filename);
+        return new Base64InputStream(new ByteArrayInputStream(normalizeString(fileData).getBytes(StandardCharsets.UTF_8)));
     }
 
     @Test
     public void testBasicVerify() throws Exception {
         String originalMessage = readFile("sample-soap-message-1.xml");
         Signature.SigningOptions options = new Signature.SigningOptions();
-        options.alias = "apigee";
+        options.alias = "my-key-alias";
         options.password = "Secret123";
+        options.jksStream = fileToInputStream("jks-base64.txt");
         Signature signer = new Signature();
         messageContent = signer.signMessage(originalMessage, options);
         System.out.printf("signed content:\n%s\n", messageContent);
 
         Map props = new HashMap<String,String>();
         props.put("debug", "true");
-        props.put("alias", "apigee");
+        props.put("alias", "my-key-alias");
         props.put("password", "Secret123");
+        props.put("jks-base64", readFile("jks-base64.txt"));
+
         SOAPVerifier callout = new SOAPVerifier(props);
 
         // execute and retrieve output
         ExecutionResult actualResult = callout.execute(msgCtxt, exeCtxt);
+        String wssec_error = msgCtxt.getVariable("wssec_error");
+        Assert.assertNull(wssec_error, "error");
+        Assert.assertEquals(actualResult, ExecutionResult.SUCCESS, "result");
         String actualContent = msgCtxt.getVariable("message.content");
         System.out.printf("ACTUAL: %s\n", actualContent);
         System.out.printf("EXPECTED: %s\n", originalMessage);
@@ -137,6 +155,107 @@ public class TestSOAPVerifierCallout {
         // this assertion does not work because of a stray unused XML ns prefix, which
         // I could not figure out how to remove.
         //Assert.assertEquals(XmlUtil.toPrettyString(actualContent), XmlUtil.toPrettyString(originalMessage));
+    }
+
+    public String lastN(String subject, int n) {
+        int length = subject.length();
+        if(length <= n){
+            return subject;
+        }
+        int startIndex = length-n;
+        return subject.substring(startIndex);
+    }
+
+
+    // I could not get the following test to work. If I pretty-print the
+    // signed output, changing whitespace in the WS-Sec HEADER, the
+    // signature validation fails.
+    //
+    // @Test
+    // public void testVerify_PrettyXml() throws Exception {
+    //     String originalMessage = readFile("1-unsigned.xml");
+    //     Signature.SigningOptions options = new Signature.SigningOptions();
+    //     options.alias = "my-key-alias";
+    //     options.password = "Secret123";
+    //     options.jksStream = fileToInputStream("jks-base64.txt");
+    //     Signature signer = new Signature();
+    //     messageContent = signer.signMessage(originalMessage, options);
+    //     System.out.printf("\n**\nsigned content:\n'%s'\n", messageContent);
+    //
+    //     // String substring1 = lastN(messageContent,332);
+    //     // // This will work only if the pretty version indents the signed portion of the content
+    //     // // in exactly the same way as the signed version.
+    //     // messageContent = XmlUtil.toPrettyString(messageContent).trim();
+    //     // System.out.printf("\npretty version:\n'%s'\n", messageContent);
+    //     // String substring2 = lastN(messageContent,332);
+    //     // System.out.printf("suffix equals?: '%s'\n", substring1.equals(substring2));
+    //
+    //     Map props = new HashMap<String,String>();
+    //     props.put("debug", "true");
+    //     props.put("alias", "my-key-alias");
+    //     props.put("password", "Secret123");
+    //     props.put("jks-base64", readFile("jks-base64.txt"));
+    //     SOAPVerifier callout = new SOAPVerifier(props);
+    //
+    //     // execute and retrieve output
+    //     ExecutionResult actualResult = callout.execute(msgCtxt, exeCtxt);
+    //     String wssec_error = msgCtxt.getVariable("wssec_error");
+    //     Assert.assertNull(wssec_error, "error");
+    //     Assert.assertEquals(actualResult, ExecutionResult.SUCCESS, "result");
+    //
+    //     String actualContent = msgCtxt.getVariable("message.content");
+    //     System.out.printf("ACTUAL: %s\n", actualContent);
+    //     System.out.printf("EXPECTED: %s\n", originalMessage);
+    // }
+
+    @Test
+    public void testVerify_UglyXml() throws Exception {
+        messageContent = readFile("1-signed-ugly.xml");
+        System.out.printf("\n**\nsigned content:\n'%s'\n", messageContent);
+
+        Map props = new HashMap<String,String>();
+        props.put("debug", "true");
+        props.put("alias", "my-key-alias");
+        props.put("password", "Secret123");
+        props.put("jks-base64", readFile("jks-base64.txt"));
+        SOAPVerifier callout = new SOAPVerifier(props);
+
+        // execute and retrieve output
+        ExecutionResult actualResult = callout.execute(msgCtxt, exeCtxt);
+        String wssec_error = msgCtxt.getVariable("wssec_error");
+        Assert.assertNull(wssec_error, "error");
+        Assert.assertEquals(actualResult, ExecutionResult.SUCCESS, "result");
+
+        String actualContent = msgCtxt.getVariable("message.content");
+        System.out.printf("ACTUAL: %s\n", actualContent);
+        String originalMessage = readFile("1-unsigned.xml");
+        System.out.printf("EXPECTED: %s\n", originalMessage);
+    }
+
+    @Test
+    public void testVerify_SemiPrettyXml() throws Exception {
+        // inserting a whitespace (newline) between SignedInfo and CanonicalizationMethod
+        // in the header causes the signature verification to fail.
+        messageContent = readFile("1-signed-semi-pretty.xml");
+        System.out.printf("\n**\nsigned content:\n'%s'\n", messageContent);
+
+        Map props = new HashMap<String,String>();
+        props.put("debug", "true");
+        props.put("alias", "my-key-alias");
+        props.put("password", "Secret123");
+        props.put("jks-base64", readFile("jks-base64.txt"));
+        SOAPVerifier callout = new SOAPVerifier(props);
+
+        // execute and retrieve output
+        ExecutionResult actualResult = callout.execute(msgCtxt, exeCtxt);
+        String wssec_error = msgCtxt.getVariable("wssec_error");
+        Assert.assertNull(wssec_error, "error");
+        Assert.assertEquals(actualResult, ExecutionResult.SUCCESS, "result");
+
+        String actualContent = msgCtxt.getVariable("message.content");
+        System.out.printf("ACTUAL: %s\n", actualContent);
+        String originalMessage = readFile("1-unsigned.xml");
+        System.out.printf("EXPECTED: %s\n", originalMessage);
     }
 
     @Test
@@ -150,11 +269,13 @@ public class TestSOAPVerifierCallout {
         props.put("jks-base64", "{non.existent.variable}");
         SOAPVerifier callout = new SOAPVerifier(props);
 
-        // execute callout - this should throw
+        // execute callout - this should generate an error
         ExecutionResult actualResult = callout.execute(msgCtxt, exeCtxt);
+        Assert.assertEquals(actualResult, ExecutionResult.ABORT, "result");
         String wssec_error = msgCtxt.getVariable("wssec_error");
 
         Assert.assertEquals(wssec_error, "empty jks-base64 property", "JKS variable");
+        //Assert.assertTrue(false, "should not be reached");
     }
 
 }
